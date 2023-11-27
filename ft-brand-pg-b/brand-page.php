217ef28e08ee2ebde40ce8ac6c09e865040a4d47
bmdominguez <bmdoming@usc.edu>
2023-11-26 19:46:19 -0800

    Update brand-page.php

--- ft-brand-pg-b/brand-page.php
+++ ft-brand-pg-b/brand-page.php
@@ -3,8 +3,11 @@
 	require '../ft-navbar-N/navbar.php';
 
   // Establish mySQL connection
-  require '../db_config.php';
-  $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  $host = "304.itpwebdev.com";
+	$user = "ethreads";
+	$pass = "460uscitp";
+	$db = "ethreads_brands_db";
+  $mysqli = new mysqli($host, $user, $pass, $db);
 
   // Check for any connection errors
   if ( $mysqli->connect_errno ) {
@@ -21,8 +24,8 @@
 
   // Retrieve results from the DB
 
-  // Retrieve brand data EXECEPT for items
-  $sql_brand_data = "SELECT brands.brand_name, brands.brand_description, brands.website, brands.instagram, brands.price_range, brands.header_image, GROUP_CONCAT(filter_name separator ', ') AS filters
+  // Retrieve brand data EXECEPT for items and filters
+  $sql_brand_data = "SELECT brands.brand_name AS brand_name, brands.brand_description AS brand_description, brands.website AS website, brands.instagram AS instagram, brands.price_range AS price, brands.header_image
   FROM brands
       LEFT JOIN filtered_brands
         ON brands.brand_id = filtered_brands.brand_id
@@ -40,9 +43,26 @@
 
   // No need for a loop, because this SQL statement will return at most 1 record
   $brand_data_row = $brand_data_results->fetch_assoc();
+
+  // Retrieve JUST associated FILTERS for that brand
+  $sql_brand_filters = "SELECT filter_name AS filters
+  FROM brands
+    LEFT JOIN filtered_brands
+      ON brands.brand_id = filtered_brands.brand_id
+    LEFT JOIN filters
+      ON filtered_brands.filter_id = filters.filter_id
+    WHERE brands.brand_id = 21;";
+
+  $brand_filters_results = $mysqli->query($sql_brand_filters);
+
+  if ( !$brand_filters_results ) {
+    echo $mysqli->error;
+    $mysqli->close();
+    exit();
+  }
       
-  // Retrieve brand data JUST for items from that brand
-  $sql_brand_items = "SELECT items.item_name, items.item_image 
+  // Retrieve brand data JUST for ITEMS from that brand
+  $sql_brand_items = "SELECT items.item_name AS item_name, items.item_image AS item_image
   FROM items
     LEFT JOIN brands
       ON items.brand_id = brands.brand_id
@@ -429,10 +449,10 @@
       style="background-image: url('brand-pg-imgs/threads.webp'); height: 300px"
     >
       <div class="px-4 py-0 my-3 text-center">
-        <div id="hero-section-title" class="display-5">HIPTIPICO</div>
+        <div id="hero-section-title" class="display-5"><?php echo $brand_data_row['brand_name']; ?></div>
         <div class="col-lg-6 mx-auto">
           <a
-            href="https://www.hiptipico.com/"
+            href="<?php echo $brand_data_row['website']; ?>"
             target="_blank"
             rel="noopener noreferrer"
             ><button type="button" id="hero-section-b" class="btn">
@@ -440,8 +460,9 @@
             </button></a
           >
         </div>
+          <!-- Might have to include if else statement for php if there is no instgram link -->
         <a
-          href="https://www.instagram.com/hiptipico/"
+          href="<?php echo $brand_data_row['instagram']; ?>"
           target="_blank"
           rel="noopener noreferrer"
           ><img
@@ -457,71 +478,46 @@
         <div class="content-box-header">At a Glance</div>
         <p class="content-p">
           Cost range:
-          <span style="color: #82a7a6; font-weight: 600"> $$ - $$$ </span>
+          <span style="color: #82a7a6; font-weight: 600">
+          <?php echo $brand_data_row['price']; ?>
+         </span>
           <br />
         </p>
         <p class="content-p">
           Has products with the following tags: &nbsp;&nbsp;
         </p>
         <div style="margin-left: 18px">
-          <span class="tag">Ethical</span>
-          <span class="tag">Handmade</span>
-          <span class="tag">Sustainable</span>
+
+          <?php while ( $brand_filters_row = $brand_filters_results->fetch_assoc() ) : ?>
+            <span class="tag"><?php echo $brand_filters_row['filters']; ?></span>
+          <?php endwhile; ?>
+
         </div>
       </div>
       <div class="container">
         <div class="content-box-header">Top Sellers</div>
         <div class="row gx-2 gy-4">
-          <div class="col">
-            <div class="item-box">
-              <img
-                class="product-img"
-                src="brand-pg-imgs/nueva-luna-strap.webp"
-                alt="La Nueva Luna Strap"
-              />
-              <br />
-              <div class="item-name">La Nueva Luna Strap</div>
+        
+            <?php while ($brand_items_row = $brand_items_results->fetch_assoc() ) : ?>
+              <div class="col">
+                <div class="item-box">
+                  <img
+                    class="product-img"
+                    src="<?php echo $brand_items_row['item_image']; ?>"
+                    alt="<?php echo $brand_items_row['item_name']; ?>"
+                  />
+                  <br />
+                  <div class="item-name"><?php echo $brand_items_row['item_name']; ?></div>
+              </div>
             </div>
-          </div>
-          <div class="col">
-            <div class="item-box">
-              <img
-                class="product-img"
-                src="brand-pg-imgs/tocoyal-hair.png"
-                alt="Tocoyal Fringe Hair Wrap (Turquoise)"
-              />
-              <br />
-              <div class="item-name">Tocoyal Fringe Hair Wrap (Turquoise)</div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="item-box">
-              <img
-                class="product-img"
-                src="brand-pg-imgs/vegan-vintage-floral-strap.png"
-                alt="Vegan Vintage Floral Strap"
-              />
-              <br />
-              <div class="item-name">Vegan Vintage Floral Strap</div>
-            </div>
-          </div>
+            <?php endwhile; ?>
+            
         </div>
       </div>
       <div class="container">
         <div class="content-box-header">About the Brand</div>
         <p class="content-p">
-          Hiptipico is an ethical fashion brand based in Panajachel, Guatemala
-          showcasing indigenous Maya artisans and their handmade creations.
-          Every Hiptipico item is thoughtfully handcrafted by local artists,
-          using high-quality sustainable materials.
-        </p>
-        <p class="content-p">
-          The name Hiptipico came from the local saying, “tipico,” which
-          describes the traditional clothing worn by the indigenous people here
-          in Guatemala. Incorporating this local context into our name and using
-          it as the basis of our brand reflects our dedication to preserving
-          indigenous culture and creating dignified job opportunities through
-          the promotion of this traditional art form.
+        <?php echo $brand_data_row['brand_description']; ?>
         </p>
       </div>
       <br />
