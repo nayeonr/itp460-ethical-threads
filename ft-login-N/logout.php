e0902c86a859d55b513b3e893cd81e9455e0b2b3
nayeonr <97630374+nayeonr@users.noreply.github.com>
2023-11-29 02:48:20 -0800

    nvm it doesnt work

--- ft-login-N/logout.php
+++ ft-login-N/logout.php
@@ -3,6 +3,6 @@
 	session_unset();
 	session_destroy();
 
-	header("Location: ../docs/ft-home-a/home.php"); // Redirect to the home page
+	header("Location: ../ft-home-a/home.php"); // Redirect to the home page
 	exit();
 ?>
